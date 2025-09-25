3eb419583ad75726162bf15e252c9fb5baff72c1
upendra-23-cmd <vrambharat05@icloud.com>
2025-09-25 13:59:24 +0530

    update for loop

--- forloop.sh
+++ forloop.sh
@@ -26,14 +26,32 @@ done
 
 
 # Given files: file1.txt, file2.txt, file3.txt,Loop through them and copy each to /backup folder.
-file=$(touch /Users/upendraverma/Desktop/programs/BajajFInserv_Practise/file1.txt file2.txt file3.txt)
-for files in $file; do
-cp $files /backup
+touch /Users/upendraverma/Desktop/programs/BajajFInserv_Practise/file1.txt file2.txt file3.txt
+for files in *.txt; do
+mkdir -p backup
+cp "$files"  backup/
 echo "files is being copied"
 done
-#
-#
-#
+
+
+# Loop through all files in the current directory and print their extensions.
+for file in *; do
+[[ -f "$file" && "$file" == *.* ]] && echo "${file##*.}"
+done
+
+# Given a list of usernames: alice bob charlie,Loop through them and print: "Hello, <username>".
+username=("alice" "bob" "john")
+for user in "${username[@]}"; do
+echo "hello $user"
+done
+
+
+# Loop through numbers 1–10 and calculate their sum
+c=0
+for i in {1..10}; do
+c=$(($c+$i))
+done
+echo $c
 
 #
 #
